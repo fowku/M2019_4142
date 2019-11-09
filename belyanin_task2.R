18cafaea3448aadfe7170b7966e7b4f5dde1a552
fowku <chek970@gmail.com>
2019-11-09 23:24:26 +0300

    completed 2 task

---
+++ belyanin_task2.R
@@ -0,0 +1,26 @@
+# Subset data by rows and columns, execute calculations for columns.
+# Returns list of subsetted dataframe and column calculated list
+subsetDataFrame <- function(dataFrame, rowSelection, columnSelection) {
+  subsettedDataFrame <- dataFrame[rowSelection, columnSelection]
+  
+  return(list(subsettedDataFrame, calculateColumns(subsettedDataFrame)))
+}
+
+# Calculates sum for numerical columns, and frequency table for strings
+calculateColumns <- function(dataFrame) {
+  calculatedResult <- list()
+  
+  for (column in names(dataFrame)) {
+    if (class(dataFrame[[column]]) == 'numeric') {
+      calculatedResult[[column]] <- sum(dataFrame[[column]])
+    }
+    
+    if (class(dataFrame[[column]]) == 'character' || class(dataFrame[[column]]) == 'logical' || class(dataFrame[[column]]) == 'factor') {
+      calculatedResult[[column]] <- table(dataFrame[[column]])
+    }
+  }
+  
+  return(calculatedResult)
+}
+
+print(subsetDataFrame(gapminder, , 1))
